8f440ed1f0b69905979e5330445f87ea9fcef912
soham <sohambasak17@gmail.com>
2026-09-22 23:33:48 +0530

    complete auth registration flow

--- auth-service/src/main/java/com/sentinelguard/auth_service/security/securityConfig.java
+++ auth-service/src/main/java/com/sentinelguard/auth_service/security/securityConfig.java
@@ -2,8 +2,10 @@ package com.sentinelguard.auth_service.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class securityConfig {
@@ -12,5 +14,16 @@ public class securityConfig {
     public PasswordEncoder passwordEncoder()
     {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http)
+    {
+        return http
+                .csrf(csrf-> csrf.disable())
+                .authorizeHttpRequests(auth->auth
+                        .requestMatchers("/auth/**").permitAll()
+                        .anyRequest().authenticated()
+                ).build();
     }
 }
